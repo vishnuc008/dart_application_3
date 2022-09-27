9bf983df80064c0e618454c0fa19f9fc13909487
vishnuc008 <vishnujishnu266@gmail.com>
2022-09-27 10:52:08 +0530

    commit

--- bin/forloop.dart
+++ bin/forloop.dart
@@ -32,5 +32,35 @@ for(int j=1;j<=10;j++){
   }
 }
 print(sum1);
-  
+print("*********************");
+//odd numbers sum
+int sum2=0;
+for(int k=1;k<=10;k++){
+  if(k%2==1){
+    sum2=sum2+k;
+  }
+}
+  print(sum2);
+  print("**************");
+  //sum of odd &even in single prgm
+  int sum3=0;
+  int sum4=0;
+  for(int r=1;r<=10;r++){
+    if(r%2==0){
+      sum3=sum3+r;
+    }
+    else{
+      sum4=sum4+r;
+    }
+    
+  }
+  print(sum3);
+  print(sum4);
+
+  print("***************");
+  //multiplication of a number
+  var mul=5;
+  for(int f=1;f<=15;f++){
+  print("$mul*$f=${mul*f}");
+  }
 }
